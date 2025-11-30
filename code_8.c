1b89b9cf83a04eeb124c854e57a56ebc7f3228d9
Soni <sonirizvi030925@eng.rizvi.edu.in>
2025-12-01 02:25:41 +0530

    Add files via upload

---
+++ code_8.c
@@ -0,0 +1,17 @@
+/*name - soni sunil chauhan
+uin - 251P061
+div - C*/
+#include<stdio.h>
+int main(){
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string : ");
+    gets(str);
+
+    while(str[i] != '\0'){
+        i++;
+    }
+    printf("Length of the string is: %d\n", i);
+    return 0;
+}
